d688e5d36062a85ca61f63959fecb5f28cdc3264
AliNiaz990487 <alik990487@gmail.com>
2024-08-19 11:43:28 +0500

    init with new apps

---
+++ app1/src/App.jsx
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1 style={{textAlign: "center", borderBottom: "2px solid"}}>Welcome from APP 1</h1>
+  )
+}
+
+export default App
---
+++ app2/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  
+
+  return (
+    <h1 style={{textAlign: "center", borderBottom: "2px solid"}}>Welcome form APP 2</h1>
+  )
+}
+
+export default App
